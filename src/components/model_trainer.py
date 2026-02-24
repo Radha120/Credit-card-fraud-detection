4a3573d4d7f8af0ff198d4732889edc4fa78b8c8
Radha krishna <arkishna2003@gmail.com>
2026-02-24 18:36:42 +0530

    Initial commit

---
+++ src/components/model_trainer.py
@@ -0,0 +1,92 @@
+import os
+import sys
+from dataclasses import dataclass
+from catboost import CatBoostRegressor
+from sklearn.ensemble import (
+    AdaBoostRegressor,
+    GradientBoostingRegressor,
+    RandomForestRegressor
+)
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+from xgboost import XGBRegressor
+from src.exception import customException
+from src.logger import logging
+from src.utils import save_object,evaluate_model
+@dataclass
+class ModelTrainerConfig:
+    trained_model_file_path=os.path.join("artifacts","model.pl")
+class ModelTrainer:
+    def __init__(self):
+        self.model_trainer_config=ModelTrainerConfig()
+    def initiate_model_trainer(self,train_array,test_array):
+        try:
+            logging.info("Split training and test input data")
+            x_train, y_train, x_test, y_test = (
+                train_array[:, :-1],
+                train_array[:, -1],
+                test_array[:, :-1],
+                test_array[:, -1]
+            )
+            models = {
+                "Linear Regression": LinearRegression(),
+                "K-Neighbors Regressor": KNeighborsRegressor(),
+                "Decision Tree": DecisionTreeRegressor(),
+                "Random Forest Regressor": RandomForestRegressor(),
+                "XGBRegressor": XGBRegressor(), 
+                "CatBoosting Regressor": CatBoostRegressor(verbose=False),
+                "AdaBoost Regressor": AdaBoostRegressor(),
+                "Gradient Boosting":GradientBoostingRegressor()
+            }
+            params={
+                "Decision Tree":{
+                    'criterion':['squared_error','friedman_mse','absolute_error','poisson']
+                },
+                "Random Forest Regressor":{
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "K-Neighbors Regressor":{
+                    'n_neighbors':[5,7,9,11]
+                },
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+                "CatBoosting Regressor":{
+                    'depth':[6,8,10],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'iterations':[30,50,100]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators':[8,16,32,64,128,256]
+                }
+            } 
+            model_report:dict=evaluate_model(x_train=x_train,y_train=y_train,x_test=x_test,y_test=y_test,models=models,param=params)
+            best_model_score = max(model_report.values())
+            best_model_name=list(model_report.keys())[
+                list(model_report.values()).index(best_model_score)
+            ]
+            best_model=models[best_model_name]
+            if best_model_score <0.6:
+                raise customException("No Best model found")
+            logging.info(f"Best found model on both training and testing dataset")
+            best_model.fit(x_train, y_train)
+            save_object(
+                file_path=self.model_trainer_config.trained_model_file_path,
+                obj=best_model
+            )
+            predicted=best_model.predict(x_test)
+            r2_square=r2_score(y_test,predicted)
+            return r2_square
+
+        except Exception as e:
+            raise customException(e,sys)
---
+++ src/utils.py
@@ -0,0 +1,39 @@
+import os
+import sys
+import numpy as np
+import pandas as pd
+import dill
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+from src.exception import customException
+def save_object(file_path,obj):
+    try:
+        dir_path=os.path.dirname(file_path)
+        os.makedirs(dir_path,exist_ok=True)
+        with open(file_path,"wb") as file_obj:
+            dill.dump(obj,file_obj)
+    except Exception as e:
+        raise customException(e,sys)
+def evaluate_model(x_train,y_train,x_test,y_test,models,param):
+    try:
+        report={}
+        for i in range(len(models)):
+            model_name = list(models.keys())[i]
+            model = list(models.values())[i]
+
+            if model_name == "CatBoosting Regressor":
+                model.fit(x_train, y_train)
+            else:
+               para = param.get(model_name, {})
+               gs = GridSearchCV(model, para, cv=3)
+               gs.fit(x_train, y_train)
+               model.set_params(**gs.best_params_)
+               model.fit(x_train, y_train)
+               y_train_pred=model.predict(x_train)
+               y_test_pred=model.predict(x_test)
+               trian_model_score=r2_score(y_train,y_train_pred)
+               test_model_score=r2_score(y_test,y_test_pred)
+               report[list(models.keys())[i]]=test_model_score
+            return report
+    except Exception as e:
+        raise customException(e,sys)
